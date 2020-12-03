e0018adfff23be57018a16b318b8243bd9131180
kseniyasharafieva <kseniyashrafieva@yandex.ru>
2020-12-04 04:28:20 +0500

    Edit index

--- bd_cars/index.php
+++ bd_cars/index.php
@@ -12,7 +12,6 @@ if (!$link) {
     exit;
 }
 ?>
-
 <h2>Список машин:</h2>
 <table border="1">
 <tr> 
@@ -40,4 +39,52 @@ $num_rows = mysqli_num_rows($result); // число записей в табли
 print("<P>Всего наименований: $num_rows </p>");
 ?>
 <p> <a href="new.php"> Добавить машину </a>
+
+<h2>Список салонов:</h2>
+<table border="1">
+<tr> 
+<th> Номер </th> <th bgcolor="#ADFF2F"> Название </th> <th> Адрес</th>
+ <th>  Редактировать </th> <th bgcolor='#DC143C'> Уничтожить </th> </tr>
+<?php
+$result= mysqli_query($link,"SELECT * FROM autosalon");
+while ($row=mysqli_fetch_array($result)){// для каждой строки из запроса
+ echo "<tr>";
+echo "<td>" . $row['id_salon'] . "</td>";
+ echo "<td bgcolor='#ADFF2F'>" . $row['name_salon'] . "</td>";
+ echo "<td>" . $row['address'] . "</td>";
+$id_salon=$row['id_salon'];
+ echo "<td><a href='edit.php?id=" . $id_salon. "'>Редактировать</a></td>"; // запуск скрипта для редактирования
+ echo "<td bgcolor='#DC143C'><a href='delete.php?id=" . $id_salon. "'>Удалить</a></td>"; // запуск скрипта для удаления записи
+ echo "</tr>";
+}
+print "</table>";
+$num_rows = mysqli_num_rows($result); // число записей в таблице БД
+print("<P>Всего наименований: $num_rows </p>");
+?>
+<p> <a href="new.php"> Добавить салон </a>
+
+<h2>Список машин в наличии:</h2>
+<table border="1">
+<tr> 
+<th> Номер </th> <th bgcolor="#ADFF2F"> Машина </th> <th> Магазин </th><th bgcolor="#ADFF2F"> Стоимость </th>
+ <th>  Редактировать </th> <th bgcolor='#DC143C'> Уничтожить </th> </tr>
+<?php
+$result= mysqli_query($link,"SELECT * FROM auto_nalich");
+while ($row=mysqli_fetch_array($result)){// для каждой строки из запроса
+ echo "<tr>";
+echo "<td>" . $row['id'] . "</td>";
+ echo "<td bgcolor='#ADFF2F'>" . $row['id_car'] . "</td>";
+ echo "<td>" . $row['id_salon'] . "</td>";
+ echo "<td bgcolor='#ADFF2F'>" . $row['cost'] . "</td>";
+$id=$row['id'];
+ echo "<td><a href='edit.php?id=" . $id. "'>Редактировать</a></td>"; // запуск скрипта для редактирования
+ echo "<td bgcolor='#DC143C'><a href='delete.php?id=" . $id. "'>Удалить</a></td>"; // запуск скрипта для удаления записи
+ echo "</tr>";
+}
+print "</table>";
+$num_rows = mysqli_num_rows($result); // число записей в таблице БД
+print("<P>Всего наименований: $num_rows </p>");
+?>
+<p> <a href="new.php"> Добавить машину </a>
+
 </body> </html>
